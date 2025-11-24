e4304be69724284934a99b89665b63955a8d7900
Anna Kroening <anjuta.kroening@gmail.com>
2025-11-23 19:34:41 -0800

    added smoke test for ci_mean

---
+++ tests/test_ci_mean.py
@@ -0,0 +1,9 @@
+from pyEpiDisplay.ci_mean import ci_mean
+
+def test_smoke():
+    """
+    Simple smoke test to make sure the function runs and returns a dict.
+    """
+    result = ci_mean([1])
+    assert isinstance(result, dict)
+    return
