6abd625df75f8347564ae03a1dc1d8b62bdbe718
Anna Kroening <anjuta.kroening@gmail.com>
2025-11-23 19:41:48 -0800

    added smoke test for ci_prop function

---
+++ tests/test_ci_prop.py
@@ -0,0 +1,9 @@
+from pyEpiDisplay.ci_prop import ci_prop
+
+def test_smoke():
+    """
+    Simple smoke test to make sure the function runs and returns a dict.
+    """
+    result = ci_prop([1])
+    assert isinstance(result, dict)
+    return
